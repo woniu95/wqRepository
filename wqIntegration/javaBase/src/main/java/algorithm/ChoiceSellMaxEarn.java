1509b5688901cbb352737e715640610379b30b2d
woniu95 <1158440401@qq.com>
2020-12-21 16:40:43 +0800

    clean doc

--- wqIntegration/javaBase/src/main/java/algorithm/ChoiceSellMaxEarn.java
+++ wqIntegration/javaBase/src/main/java/algorithm/ChoiceSellMaxEarn.java
@@ -1,14 +1,12 @@
 package algorithm;
 
-import java.util.ArrayList;
-
 /**
- *   每次卖更新最大利润
- *   MAX( 之前卖最大总共赚+ 卖那天之后截止今天最大总共赚,  直接一次买卖赚（今天-最小-2）)
- *                   2  0  6  -4 5
- *                [0, 0, 2, 2, 4, 4, 5,     4|4]
+ * 714
+ *
+ * 之前卖最大总共赚+ 卖那天之后截止今天最大总共赚,  直接一次买卖赚（今天-最小-2）)
  *
  * 输入: prices = [1, 3, 5, 5, 9, 11, 14, 16], fee = 2
+ *
  * 输出: 8
  * 解释: 能够达到的最大利润:
  * 在此处买入 prices[0] = 1
@@ -19,7 +17,6 @@ import java.util.ArrayList;
  *
  * Max(sum(sell[j] - buy[i] - 2)) 0 < i < j < prices.length
  *
- * 是否持有 是 无法买入，可以卖出 否 可以买入，不能卖出
  *
  * @program: wqIntegration
  * @description:
@@ -32,68 +29,39 @@ public class ChoiceSellMaxEarn {
 
         int[] prices = {1, 4, 2, 8, 4, 9};
         int fee = 2;
-        int hold = 0;
-        ArrayList<Integer> operates = new ArrayList<Integer>();
-        int day = 0;
-        int preSellDay = 0;
-        int preSellCanEarn = 0;
-        int dayCanEarn = 0;
-        int preMin = 0;
-        while(day<prices.length){
-
-
-            if(preMin>prices[day]){
-                preMin = prices[day];
-            }
-
-            if(preSellDay>0){
-                int afterCanEarn = 0;
-                if(preSellDay+1 < day){
-                    int min = prices[preSellDay+1];
-                    for(int i= preSellDay+1;i<day;i++){
-                        if(prices[i]<min){
-                            min = prices[i];
-                        }
-                    }
-                    afterCanEarn = prices[day] - min -fee;
-                }
-
-                int earn = 0;
-
-                if(prices[day] > prices[preSellDay]+fee){
-                    earn = prices[day] - prices[preSellDay];
-                }
-                dayCanEarn = Math.max(earn+preSellCanEarn, afterCanEarn+preSellCanEarn);
-
-                dayCanEarn = Math.max(dayCanEarn, prices[day] - preMin);
-
-                if(dayCanEarn > preSellCanEarn){
-                    preSellDay = day;
-                    preSellCanEarn = dayCanEarn;
-                }
-
-            }else{
-                int i=1;
-                while(day+i<prices.length){
-                    if(prices[day+i] - prices[day+i-1]-2 > 0){
-
-                        preSellCanEarn = prices[day+i] - prices[day+i-1]-2;
-                        preSellDay = day+i;
-                        day = day+i-1;
-
-                        break;
-                    }
-                }
-
-            }
-
-            day++;
-        }
-        System.out.println(preSellCanEarn);
-        System.out.println(getMaxEarnUseBrute(0,0,0, prices));
+        System.out.println(getMaxEarnUseDP(prices, fee));
 
     }
 
+    /**
+     * 是否持有
+     *      是 无法买入，可以卖出
+     *      否 可以买入，不能卖出
+     *
+     * dp[i][0]: 第i天手上无持有情况最大利润, dp[i][1]:第i天手上持有情况最大利润
+     * dp[i+1][0] = Max(dp[i][0], dp[i][1]+prices[i]-fee)
+     * dp[i+1][1] = Max(dp[i][0]-prices[i+1], dp[i][1])
+     * @param prices
+     * @param fee
+     * @return
+     */
+    static int getMaxEarnUseDP(int[] prices, int fee){
+        int maxEarn = 0;
+
+        int day =0;
+        int holdEarn = -prices[day];
+        int notHoldEarn = 0;
+        day++;
+        while(day<prices.length){
+            int preHoldEarn = holdEarn;
+            int preNotHoldEarn = notHoldEarn;
+            holdEarn = Math.max(preHoldEarn, preNotHoldEarn-prices[day]);
+            notHoldEarn = Math.max(preNotHoldEarn, preHoldEarn+prices[day]-fee);
+            maxEarn = Math.max(holdEarn, notHoldEarn);
+            day++;
+        }
+        return maxEarn;
+    }
 //    0:nothing 1:buy 2:sell
     static int getMaxEarnUseBrute(int day, int hold, int preEarn, int[] prices){
 
