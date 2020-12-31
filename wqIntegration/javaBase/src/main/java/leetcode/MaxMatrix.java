2814b8fa7eafc5c9716ca53dcea665da3b8004ef
woniu95 <1158440401@qq.com>
2020-12-31 11:57:52 +0800

    clean doc

--- wqIntegration/javaBase/src/main/java/leetcode/MaxMatrix.java
+++ wqIntegration/javaBase/src/main/java/leetcode/MaxMatrix.java
@@ -39,20 +39,69 @@ package leetcode;
  */
 public class MaxMatrix {
     public static void main(String[] args) {
-        int[][] arr = {{0,1,0,0},{1,0,1,1,1},{1,1,1,1,1},{1,0,0,1,0}};
-        System.out.println(getMaxMatrixUseBrute(arr));
-        System.out.println(getMaxMatrixUseBrutePro(arr));
+//        int[][] arr = {{1,0,1,1},{1,1,1,0,1},{1,1,1,0,1},{1,1,0,1,0}};
+        int i=0;
+        do{
+            char[][] arr = getTemple();
+            System.out.print(getMaxMatrixUseBrute(arr)+"  , ");
+            System.out.println(getMaxMatrixUseBrutePro(arr));
+        }while (i++<100);
+
 
     }
 
-    public static int getMaxMatrixUseBrutePro(int[][] arr){
+    public static char[][] getTemple(){
+        int rowL = (int)(Math.random()*100+1);
+        int colL = (int)(Math.random()*100+1);
+        char [][] tem = new char[rowL][colL];
+        for(int i=0;i<rowL;i++){
+            for(int j=0;j<colL;j++){
+                int flag = (int)(Math.random()*10);
+                if(flag>5){
+                    tem[i][j] = '1';
+                }else{
+                    tem[i][j] = '0';
+                }
+            }
+        }
+        return tem;
+    }
+    public static int getMaxMatrixUseBrutePro(char[][] arr){
         int max = 0;
 
+        for(int i=0,rowLength = arr.length; i<rowLength;i++){
+            for(int j=0,colLength = arr[0].length;j<colLength;j++){
+                if(arr[i][j] == '0'){
+                    continue;
+                }
+                int[][] dp = new int[arr.length][arr[0].length];
+                for(int r=i;r<rowLength;r++){
+                    for(int c=j;c<colLength;c++){
+                        if(arr[r][c] == '0'){
+                            dp[r][c] = 0;
+                        }else if((r-1>=i && c-1>=j) && (dp[r][c-1] == 0 || dp[r-1][c] == 0)){
+                            dp[r][c] = 0;
+                        }else if(r==i && c-1>=j && dp[r][c-1] == 0){
+                            dp[r][c] = 0;
+                        }else if(r-1>=i && c==j && dp[r-1][c] == 0){
+                            dp[r][c] = 0;
+                        }else{
+                            dp[r][c] = (r+1-i)*(c+1-j);
+                            max = Math.max(dp[r][c], max);
+                        }
+                        if(dp[r][c] == 0){
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
         return max;
     }
 
 
-    public static int getMaxMatrixUseBrute(int[][] arr){
+    public static int getMaxMatrixUseBrute(char[][] arr){
         int max = 0;
         for(int i=0,rowLength = arr.length; i<rowLength;i++){
             for(int j=0,colLength = arr[0].length;j<colLength;j++){
@@ -66,12 +115,12 @@ public class MaxMatrix {
         return max;
     }
 
-    public static int getArea(int x, int y, int x2, int y2, int[][] arr){
+    public static int getArea(int x, int y, int x2, int y2, char[][] arr){
         int area = 0;
         for(int i=x; i<=x2;i++){
             for(int j=y;j<=y2;j++){
-                if(arr[i][j] == 0){
-                    return 0;
+                if(arr[i][j] == '0'){
+                    return -1;
                 }else{
                     area++;
                 }
