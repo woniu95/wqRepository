98792302fbff2c26d9804b6d0d46633a1314244a
xianchou <1158440401@qq.com>
2021-05-22 13:59:42 +0800

    reference

--- wqIntegration/javaBase/src/main/java/JavaReference/ReferenceTest.java
+++ wqIntegration/javaBase/src/main/java/JavaReference/ReferenceTest.java
@@ -1,6 +1,8 @@
 package JavaReference;
 
 import java.lang.ref.*;
+import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -27,35 +29,63 @@ import java.lang.ref.*;
  * 启用该选项，当出现java.lang.OutOfMemoryError异常时，Java的堆信息会dump到当前目录的文件中，文件命名为： java_pid[进程PID].hprof
  *
  *
+ * 弱引用
+ * 发生GC时  当对象只有弱引用指向时, GC 会回收该对象内存，并把对应的弱引用放入指定的ReferenceQueue中， 方便清除弱引用对象
+ * eg: WeakHashMap.Entry extends WeakReference 构造方法调用  super(key, queue);把key指定为WeakReference, 当key对象无强引用后,
+ * GC会把该Entry放入指定的ReferenceQueue中.  在WeakHashMap.expungeStaleEntries 方法中清除无用的Entry和Entry.value;
+ *
+ *
+ * 软引用
+ * 当对象只被SoftReference 软引用指向时, 当内存不够，发生OutOfMemoryException之前会该对象堆空间释放，并把对应的弱引用放入指定的ReferenceQueue中.
+ * 以上两种引用 通常用于 解决缓存OOM 问题， 常见方式： Map<key, SoftReference>   SoftReference<CacheItem>
+ * 通过ReferenceQueue<SoftReference> 清除SoftReference无效引用（map中的）
+ *
  */
 public class ReferenceTest {
-    public static void main(String[] args){
-        ReferenceQueue<Object> refQueue = new ReferenceQueue<Object>();
-        ReferenceQueue<Object> refQueue1 = new ReferenceQueue<Object>();
-        // 强引用
-        String weakObj = new String("weak reference String");
-        String softObj = new String("soft reference String");
 
-        //自定义弱引用
-        //发生GC时  当对象只有弱引用指向时, GC 会回收该对象内存，并把对应的弱引用放入指定的ReferenceQueue中， 方便清除弱引用对象
-        // eg: WeakHashMap.Entry extends WeakReference 构造方法调用  super(key, queue);把key指定为WeakReference, 当key对象无强引用后,
-        // GC会把该Entry放入指定的ReferenceQueue中.  在WeakHashMap.expungeStaleEntries 方法中清除无用的Entry和Entry.value;
+    private static volatile boolean isRun = true;
 
-        WQWeakReference customWeak = new  WQWeakReference(weakObj, refQueue);
+    public static void main(String[] args) throws InterruptedException {
 
-        // 软引用
-        // 当对象只被SoftReference 软引用指向时, 当内存不够，发生OutOfMemoryException之前会该对象堆空间释放，并把对应的弱引用放入指定的ReferenceQueue中.
-        // 暂未测试出符合的结果
-        // 以上两种引用 通常用于 解决缓存OOM 问题， 常见方式： Map<key, SoftReference>   SoftReference<CacheItem>
-        // 通过ReferenceQueue<SoftReference> 清除SoftReference无效引用（map中的）
-        SoftReference soft = new SoftReference(softObj, refQueue1);
 
-        weakObj = null;
-        softObj = null;
-        System.gc();
-        System.out.println("Weak Reference: "+refQueue.poll().toString());
-        Object[] requstMemory = new Object[1024*60];
-        System.out.println("Soft Reference: "+refQueue1.poll());
+        ReferenceQueue<Object> weakRefQueue = new ReferenceQueue<Object>();
+        ReferenceQueue<Object> softRefQueue = new ReferenceQueue<Object>();
+        ReferenceQueue<Object> phantomRefQuene = new ReferenceQueue<>();
+        //检测各种引用类型队列
+        new Thread(() -> {
+            while (isRun)
+            {
+
+                getFromReferenceQueue(weakRefQueue);
+                getFromReferenceQueue(softRefQueue);
+                getFromReferenceQueue(phantomRefQuene);
+
+            }
+        }).start();
+
+        getReference(10, "WEAK", weakRefQueue);
+        getReference(10, "SOFT", softRefQueue);
+        getReference(10, "PHANTOM", phantomRefQuene);
+
+        //申请内存使OutOfMemoryError
+        byte[] requstMemory;
+        try{
+            System.out.println("Current memory total: "+ Runtime.getRuntime().totalMemory()/1024/8);
+
+            System.out.println("Allocate new byte[1024*60] after 5 second");
+            TimeUnit.SECONDS.sleep(5);
+            int freeMemory = (int) Runtime.getRuntime().freeMemory();
+            requstMemory = new byte[freeMemory+100];
+            System.out.println("new byte["+freeMemory+100+"]");
+            System.out.println("Current memory total: "+ Runtime.getRuntime().totalMemory()/1024/8);
+
+            System.out.println("fill in all byte[1024*60]");
+            System.out.println("Current memory total: "+ Runtime.getRuntime().totalMemory()/1024/8);
+        }catch (OutOfMemoryError e){
+            e.printStackTrace();
+        }
+
+
     }
 
     static class  WQWeakReference<K> extends WeakReference<K>{
@@ -68,5 +98,64 @@ public class ReferenceTest {
         public String toString() {
             return "WQWeakReference{}, string:"+k;
         }
+    }
+
+    static void getFromReferenceQueue(ReferenceQueue referenceQueue){
+        Object ref = referenceQueue.poll();
+        if (ref != null)
+        {
+            try
+            {
+                Field referent = Reference.class.getDeclaredField("referent");
+                referent.setAccessible(true);
+                Object result = referent.get(ref);
+
+                System.out.println("==============>reference:" + ref.getClass() + "@" + ref.hashCode());
+                if(result == null){
+                    System.out.println("==============>referent: has bean null, gc will collect:");
+                }else{
+                    System.out.println("==============>referent: not null.  " + result.getClass() + "@" + result.hashCode());
+
+                }
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     *
+     * @param size 单位 M
+     * @param type
+     * @param referenceQueue
+     */
+    static Reference getReference(int size, String type, ReferenceQueue referenceQueue) throws InterruptedException {
+
+        System.out.println("getReference size: "+size+", type:" + type);
+        //用reference 保持对refMemory对应类型的引用
+        // 如果直接new *Reference(refMemory, referenceQueue) ,会使refMemory无引用指向, 直接变成不可达, 被GC直接回收，
+        // 不会放入referenceQueue中
+        Reference reference = null;
+        byte[] refMemory = new byte[1024*1024*size];
+        switch (type){
+            case "SOFT":
+                reference = new SoftReference(refMemory, referenceQueue);
+                break;
+            case "WEAK":
+                reference = new WeakReference(refMemory, referenceQueue);
+                break;
+            case "PHANTOM":
+                reference = new PhantomReference(refMemory, referenceQueue);
+        }
+        //清除强引用
+        refMemory = null;
+        System.out.println("==============>sleep 3 second before System.gc()");
+        TimeUnit.SECONDS.sleep(3);
+        System.gc();
+        System.out.println("==============>called System.gc()");
+        System.out.println();
+        System.out.println();
+        return reference;
     }
 }
--- wqIntegration/javaBase/src/main/java/Main.java
+++ wqIntegration/javaBase/src/main/java/Main.java
@@ -1,7 +1,38 @@
+import java.util.*;
+
 public class Main{
 
     public static void main(String[] args) throws InterruptedException {
 
+       HashMap map = new HashMap();
+       map.put("1", "a");
+       int hash = 16;
+       int length = 32;
+       int mask = length -1;
+       System.out.println(Integer.toBinaryString(hash));
+       System.out.println(Integer.toBinaryString(length));
+       System.out.println(Integer.toBinaryString(mask));
+       System.out.println(hash&length);
+       System.out.println( hash % length);
+       System.out.println( hash & mask);
+
+        /**1234
+         * 8
+         * 00000010000
+         * 00000010000
+         * 00000001111
+         * 0
+         * 2
+         *
+         */
+
+    }
+    static class OBj{
+        public String a;
+
+        public OBj(String a) {
+            this.a = a;
+        }
 
     }
 
