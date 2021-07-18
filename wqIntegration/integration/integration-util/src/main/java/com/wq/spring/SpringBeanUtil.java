a6d97127c188f4ae379c38e22707ce811c4126fb
xianchou <1158440401@qq.com>
2021-07-18 10:24:49 +0800

    reference

---
+++ wqIntegration/integration/integration-util/src/main/java/com/wq/spring/SpringBeanUtil.java
@@ -0,0 +1,23 @@
+package com.wq.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
+
+/**
+ * @description: 获取spring组件的工具类
+ */
+@Service
+public class SpringBeanUtil implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringBeanUtil.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> tClass) {
+        return applicationContext.getBean(tClass);
+    }
+}
